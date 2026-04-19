420dfd2bb6028a5a9471076f73e5ecef0250c4c2
Deiby Deaquiz <alxds.ad@gmail.com>
2026-04-19 02:55:19 -0500

    fix: eliminacion  iconos

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
 
-  // 🌐 CORS
+  // CORS
   app.enableCors();
 
-  // 🧪 ValidationPipe
+  // ValidationPipe
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -22,12 +22,12 @@ async function bootstrap() {
     }),
   );
 
-  // 📚 Swagger config
+  // Swagger config
   const config = new DocumentBuilder()
     .setTitle('Auth Service')
     .setDescription('Microservicio de autenticación con JWT')
     .setVersion('1.0')
-    .addBearerAuth() // 🔐 habilita JWT en Swagger
+    .addBearerAuth() // habilita JWT en Swagger
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
@@ -39,8 +39,8 @@ async function bootstrap() {
 
   await app.listen(port);
 
-  console.log(`🚀 Running on http://localhost:${port}`);
-  console.log(`📚 Swagger available on http://localhost:${port}/docs`);
+  console.log(`Running on http://localhost:${port}`);
+  console.log(`Swagger available on http://localhost:${port}/docs`);
 }
 
 bootstrap();
